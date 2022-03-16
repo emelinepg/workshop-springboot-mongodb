65aed5fb9f36cea5ee39881ba9176e308d7d0b4d
emelinepg <emelinedev84@gmail.com>
2022-03-16 19:42:32 -0300

    Get post by id

---
+++ src/main/java/com/devnoir/workshopmongo/resources/PostResource.java
@@ -0,0 +1,62 @@
+package com.devnoir.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devnoir.workshopmongo.domain.Post;
+import com.devnoir.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService postService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll() {
+		List<Post> list = postService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = postService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	/*@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPostByUserId(@PathVariable String id) {
+		User obj = userService.findById(id);
+		List<Post> list = obj.getPosts();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO dto) {
+		User obj = userService.fromDTO(dto);
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO dto, @PathVariable String id) {
+		User obj = userService.fromDTO(dto);
+		obj.setId(id);
+		obj = userService.update(obj);
+		return ResponseEntity.noContent().build();
+	}*/
+}
---
+++ src/main/java/com/devnoir/workshopmongo/services/PostService.java
@@ -0,0 +1,51 @@
+package com.devnoir.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devnoir.workshopmongo.domain.Post;
+import com.devnoir.workshopmongo.repositories.PostRepository;
+import com.devnoir.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public List<Post> findAll() {
+		return postRepository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	/*public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return userRepository.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
+	public User fromDTO(UserDTO dto) {
+		return new User(dto.getId(), dto.getName(), dto.getEmail());
+	}*/
+}
