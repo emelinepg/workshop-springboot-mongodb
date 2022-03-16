4909be98738e1e4ebe6fe9ade16b49a0bd0efb94
emelinepg <emelinedev84@gmail.com>
2022-03-16 15:39:43 -0300

    Finding user by id

--- src/main/java/com/devnoir/workshopmongo/services/UserService.java
+++ src/main/java/com/devnoir/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.devnoir.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devnoir.workshopmongo.domain.User;
 import com.devnoir.workshopmongo.repositories.UserRepository;
+import com.devnoir.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
