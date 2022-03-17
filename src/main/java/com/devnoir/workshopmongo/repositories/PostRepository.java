e16661fa2d8a243442d62fe8ecd05758e905c776
emelinepg <emelinedev84@gmail.com>
2022-03-16 21:10:03 -0300

    @Query

--- src/main/java/com/devnoir/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/devnoir/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.devnoir.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devnoir.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.devnoir.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	public List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
